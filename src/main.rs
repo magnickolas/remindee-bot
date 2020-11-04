27f47931d9ae60fa2e688b670bcf8f8f6d6ba3e0
Nikolai Oplachko <magnickolas@gmail.com>
2020-11-04 11:10:28 +0300

    split mod

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,14 @@
 extern crate lazy_static;
 
 mod db;
+mod tg;
 
 use async_std::task;
-use chrono::offset::{FixedOffset, TimeZone};
+use chrono::offset::FixedOffset;
 use chrono::prelude::*;
-use chrono::Utc;
-use regex::Regex;
 use std::time::Duration;
 use teloxide::dispatching::update_listeners::polling_default;
 use teloxide::prelude::*;
-use teloxide::types::Message;
-use teloxide::types::ParseMode::MarkdownV2;
 use teloxide::types::UpdateKind;
 use teloxide::utils::markdown::{bold, escape};
 use tokio::runtime::Handle;
@@ -30,45 +27,11 @@ impl ToString for db::Reminder {
     }
 }
 
-async fn send_message(text: &String, bot: &Bot, user_id: i64) {
-    bot.send_message(user_id, text)
-        .parse_mode(MarkdownV2)
-        .send()
-        .await
-        .map(|_| ())
-        .unwrap_or_else(|err| {
-            dbg!(err);
-        })
-}
-
-fn parse_req(s: &str, msg: &Message) -> Option<db::Reminder> {
-    lazy_static! {
-        static ref RE: Regex =
-            Regex::new(r"^(?P<hour>\d{1,2}):(?P<minutes>\d{2})\s*(?P<desc>.*?)\s*$").unwrap();
-    }
-    RE.captures(s).map(|caps| {
-        //TODO remove fixed offset
-        let now = FixedOffset::east(3 * 3600).from_utc_datetime(&Utc::now().naive_utc());
-        let time = now.date().and_hms(
-            caps["hour"].to_string().parse().unwrap(),
-            caps["minutes"].to_string().parse().unwrap(),
-            0,
-        );
-        db::Reminder {
-            id: 0,
-            user_id: msg.chat_id(),
-            time: time.with_timezone(&Utc),
-            desc: caps["desc"].to_string(),
-            sent: false,
-        }
-    })
-}
-
 async fn reminders_pooling(bot: &Bot) {
     loop {
         let reminders = db::get_active_reminders().unwrap();
         for reminder in reminders {
-            send_message(&reminder.to_string(), &bot, reminder.user_id).await;
+            tg::send_message(&reminder.to_string(), &bot, reminder.user_id).await;
             db::mark_reminder_as_sent(&reminder).unwrap()
         }
         task::sleep(Duration::from_secs(1)).await;
@@ -107,14 +70,18 @@ async fn run() {
                                     .unwrap_or(
                                         "Error occured while querying reminders...".to_string(),
                                     );
-                                send_message(&text, &bot, msg.chat_id()).await
+                                tg::send_message(&text, &bot, msg.chat_id()).await
                             }
-                            text => match parse_req(text, &msg) {
+                            text => match tg::parse_req(text, &msg) {
                                 Some(reminder) => {
                                     let res = db::insert_reminder(&reminder);
                                     if res.is_ok() {
-                                        send_message(&escape("Remember that!"), &bot, msg.chat_id())
-                                            .await
+                                        tg::send_message(
+                                            &escape("Remember that!"),
+                                            &bot,
+                                            msg.chat_id(),
+                                        )
+                                        .await
                                     }
                                     res.unwrap_or_else({
                                         |err| {
@@ -124,7 +91,7 @@ async fn run() {
                                 }
                                 None => match msg.from() {
                                     Some(user) if user.id as i64 == msg.chat_id() => {
-                                        send_message(
+                                        tg::send_message(
                                             &escape("Incorrect request!"),
                                             &bot,
                                             msg.chat_id(),
---
+++ src/tg.rs
@@ -0,0 +1,42 @@
+use crate::db;
+
+use chrono::offset::{FixedOffset, TimeZone};
+use chrono::Utc;
+use regex::Regex;
+use teloxide::prelude::*;
+use teloxide::types::Message;
+use teloxide::types::ParseMode::MarkdownV2;
+
+pub async fn send_message(text: &String, bot: &Bot, user_id: i64) {
+    bot.send_message(user_id, text)
+        .parse_mode(MarkdownV2)
+        .send()
+        .await
+        .map(|_| ())
+        .unwrap_or_else(|err| {
+            dbg!(err);
+        })
+}
+
+pub fn parse_req(s: &str, msg: &Message) -> Option<db::Reminder> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"^(?P<hour>\d{1,2}):(?P<minutes>\d{2})\s*(?P<desc>.*?)\s*$").unwrap();
+    }
+    RE.captures(s).map(|caps| {
+        //TODO remove fixed offset
+        let now = FixedOffset::east(3 * 3600).from_utc_datetime(&Utc::now().naive_utc());
+        let time = now.date().and_hms(
+            caps["hour"].to_string().parse().unwrap(),
+            caps["minutes"].to_string().parse().unwrap(),
+            0,
+        );
+        db::Reminder {
+            id: 0,
+            user_id: msg.chat_id(),
+            time: time.with_timezone(&Utc),
+            desc: caps["desc"].to_string(),
+            sent: false,
+        }
+    })
+}
