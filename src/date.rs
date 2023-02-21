81e69ac1171e8498c07eae221c7543c583cb69f4
Nikolai Oplachko <magnickolas@gmail.com>
2023-02-21 08:11:11 +0400

    implement add_months helper function

--- src/date.rs
+++ src/date.rs
@@ -1,3 +1,7 @@
+use std::cmp::min;
+
+use chrono::{Datelike, NaiveDate};
+
 fn is_leap_year(year: i32) -> bool {
     year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)
 }
@@ -17,4 +21,12 @@ pub fn days_in_year(year: i32) -> u32 {
     } else {
         365
     }
+}
+
+pub fn add_months(date: &NaiveDate, months: u32) -> NaiveDate {
+    let total_month = date.month() + months;
+    let year = date.year() + total_month as i32 / 12;
+    let month = total_month % 12;
+    let day = min(date.day(), days_in_month(month, year));
+    NaiveDate::from_ymd_opt(year, month, day).unwrap()
 }
