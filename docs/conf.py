506186efbbff2c9bbc9d37d0675468d688ed1da0
Nikolai Oplachko <magnickolas@gmail.com>
2023-10-12 14:29:15 +0200

    ignore ds_store files

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'remindee-bot'
 copyright = '2023, Nikolai Oplachko'
 author = 'Nikolai Oplachko'
-release = '0.2.3'
+release = '0.2.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
