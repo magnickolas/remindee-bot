e80ff387b5eb553eee77d9c84f108245b69322e2
Nikolai Oplachko <magnickolas@gmail.com>
2024-07-05 14:32:24 +0200

    show year of reminder date if not the current one

--- src/generic_reminder.rs
+++ src/generic_reminder.rs
@@ -34,7 +34,11 @@ pub trait GenericReminder {
         let now = Utc::now().with_timezone(&user_timezone);
         let mut s = String::new();
         if time.date_naive() != now.date_naive() {
-            s += &format!("{:02}.{:02} ", time.day(), time.month());
+            s += &format!("{:02}.{:02}", time.day(), time.month());
+            if time.year() != now.year() {
+                s += &format!(".{}", time.year())
+            }
+            s += " "
         }
         s + &format!("{:02}:{:02}", time.hour(), time.minute())
     }
